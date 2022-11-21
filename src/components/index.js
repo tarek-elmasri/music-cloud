22e73befbcccfd00746187ea6a58ed55d36ab281
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-11-21 15:15:48 +0300

    [feat] construct Discover page with its essential components

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,15 @@
 import Sidebar from './Sidebar';
 import SearchBar from './Searchbar'
 import TopPlay from './TopPlay'
+import Error from './Error'
+import Loader from './Loader'
+import SongCard from './SongCard'
 
 export {
   Sidebar,
   SearchBar,
-  TopPlay
+  TopPlay,
+  Error,
+  Loader,
+  SongCard
 };
