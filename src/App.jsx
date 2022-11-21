fa31433cff6cecc9bc27865f23aca2544feb1ca8
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-11-21 13:33:35 +0300

    [core] set main page structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from "react";
-import { Sidebar } from "./components";
+import { Route, Routes } from "react-router-dom";
+import { SearchBar, Sidebar, TopPlay } from "./components";
+import { Discover } from "./pages";
 
 const App = () => {
   return (
     <div className="relative flex">
       <Sidebar />
+
+      <div className="flex-1 flex flex-col bg-gradient-to-br from-black to-[#121286]">
+        <SearchBar />
+
+        <div className="px-6 h-[calc(100vh-72px)] overflow-y-scroll hide-scrollbar flex xl:flex-row flex-col-reverse">
+          <div className="flex-1 h-fit pb-40">
+            <Routes>
+              <Route path="/" element={<Discover />} />
+            </Routes>
+          </div>
+          <div className="xl:sticky relative top-0 h-fit">
+            <TopPlay />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,9 @@
 import Sidebar from './Sidebar';
+import SearchBar from './Searchbar'
+import TopPlay from './TopPlay'
 
 export {
   Sidebar,
+  SearchBar,
+  TopPlay
 };
