9542a95ec4139485af4f7c219cd96f14669f0abf
tarek-elmasry <dr.tareqelmasry@hotmail.com>
2022-11-22 16:15:35 +0300

    [chore] add Track Component

--- src/components/MusicPlayer/index.jsx
+++ src/components/MusicPlayer/index.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import { useSelector } from "react-redux";
+import Track from "./Track";
 
 const MusicPlayer = () => {
+  const { isActive, isPlaying, activeSong } = useSelector(
+    (state) => state.player
+  );
+
   return (
     <div className="relative sm:px-12 px-8 w-full flex justify-between items-center">
-      <div> Track</div>
+      <Track
+        isPlaying={isPlaying}
+        isActive={isActive}
+        activeSong={activeSong}
+      />
       <div className="flex-1 flex flex-col items-center justify-center">
         <div>controls</div>
         <div>bar</div>
